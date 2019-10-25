2fad594d5cf619439ee81b5af6b1b55aed667025
Joseph Pyram <joseph.pyram@gmail.com>
2019-10-25 09:01:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,7 +34,9 @@ def long_planeteer_calls (array)   #code an argument here
     false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  found = array.include?(cheese_types[0])
 end
