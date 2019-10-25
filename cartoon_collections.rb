fc31624c8c26116901c90f1f0938f44f055ffa26
Joseph Pyram <joseph.pyram@gmail.com>
2019-10-25 09:23:41 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,21 +1,53 @@
 def roll_call_dwarves (dwarves) # code an argument here
-  #dwarves = ["Doc", "Grumpy", "Happy", "Sleepy", "Dopey", "Bashful", "Sneezy"]
-  array = dwarves.sort
-  array.size.times do |n|
-    puts "#{n+1}.*#{array[n]}"
-  end
+#Solution without hashes
+#  dwarves.size.times do |n|
+#    puts "#{n+1}.*#{dwarves[n]}"
+#  end
+
+#Solution using Hashes
+  hash = Hash.new
+  dwarves.each_with_index { |item, index|
+      hash[item] = index
+      puts "#{index+1}.*#{hash.key(index)}"
+  }
+  
 end
   
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (array)  # code an argument here
+    array.map do |string|
+       string.capitalize + "!"
+    end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls (array)   #code an argument here
+    one_greater_4 = array.find {|string| string.size > 4}
+    all_less_equal_4 = array.all? {|word| word.length <= 4}
+
+    if (one_greater_4)
+        return true 
+    elseif all_less_equal_4
+        return false
+    end
+    
+    false
 end
 
-def find_the_cheese# code an argument here
+
+def find_the_cheese (array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  a = cheese_types[0]
+  b = cheese_types[1]
+  c = cheese_types[2]
+  found = 0
+  criteria_met = array.include?(a) || array.include?(b) || array.include?(c)
+
+  if criteria_met
+      found = array.find {|word| word==a || word==b || word==c}
+      return found
+  end
+  
+  nil
 end
